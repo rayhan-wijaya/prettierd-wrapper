5c3f6036418ae68bcca893b0878b19e3f667afd3
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-12-22 18:36:06 +0700

    corrected quotes

--- prettierd_wrapper.bash
+++ prettierd_wrapper.bash
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 hash prettierd 2>/dev/null || {
-    echo >&2 "prettierd could not be found; please install..."
+    echo >&2 'prettierd could not be found; please install...'
     exit 1
 }
 
-if [ "$1" = "" ]
+if [ "$1" = '' ]
 then
-    echo "please provide a file..."
+    echo 'please provide a file...'
     exit 1
 fi
 
 res=$(cat $1 | prettierd $1)
 
-if [ "$?" = "0" ]
+if [ "$?" = '0' ]
 then
     echo $res > $1
 else
-    echo "something went wrong..."
+    echo 'something went wrong...'
     echo $res
 
     exit 1
